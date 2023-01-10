d4c4112e0d00c23214b677cb719bad7e55fe5f83
오근혁 <oyfkr@naver.com>
2023-01-10 23:39:08 +0900

    [수정] 빠진 부분 추가..

--- soomgo/src/main/java/project/soomgo/configure/SecurityConfig.java
+++ soomgo/src/main/java/project/soomgo/configure/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 // 로그인, 회원가입 api 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
                 .authorizeRequests()
-                .antMatchers("/auth/**", "/swagger*/**").permitAll()
+                .antMatchers("/auth/**", "/swagger*/**", "/v2/api-docs").permitAll()
                 .anyRequest().authenticated()
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스 적용
