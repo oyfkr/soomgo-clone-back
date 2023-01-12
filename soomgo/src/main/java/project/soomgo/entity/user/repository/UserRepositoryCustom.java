7b52b34e589e6edccd39caaa6b331e7dd44f6cab
오근혁 <oyfkr@naver.com>
2023-01-13 00:07:55 +0900

    패키지 이동 후 커밋

--- soomgo/src/main/java/project/soomgo/entity/user/repository/UserRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package project.soomgo.entity.user.repository;
-
-import java.util.Optional;
-import project.soomgo.entity.user.Users;
-
-public interface UserRepositoryCustom {
-
-    Optional<Users> findByEmail(String email);
-}
--- soomgo/src/main/java/project/soomgo/entity/user/repository/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package project.soomgo.entity.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.soomgo.entity.user.Users;
-
-public interface UsersRepository extends JpaRepository<Users, Long> , UserRepositoryCustom{
-
-}
